47fe8795ee0b9975b69cfbaf9561f6b73048d2a5
daniel <dans44684@hotmail.com>
2023-01-02 10:46:36 +0900

    Add refresh token to oauth flows

--- lib/oauth/oauth.ex
+++ lib/oauth/oauth.ex
@@ -2,13 +2,50 @@ defmodule OAuth do
   @doc "Define the uri the user must log to"
   @callback uri :: String.t()
 
-  @doc "Exchange a oauth code for an access token"
-  @callback exchange_code!({atom(), String.t()}) :: String.t()
+  @doc "Exchange a oauth code for oauth access and refresh tokens"
+  @callback exchange_code!({atom(), String.t()}) :: %{
+              access_token: String.t(),
+              refresh_token: String.t()
+            }
 
-  def create_access_token!(impl) do
+  @doc "Get a new access token from a refresh token"
+  @callback refresh_access_token!(String.t()) :: String.t()
+  @doc "Look for a refresh token from the configuration"
+  @callback get_refresh_token_from_env() :: String.t() | nil
+
+  def access_token!(impl) do
+    %{access_token: access_token} =
+      get_from_cache(fn cache -> cache[impl] end) || refresh_access_token!(impl) ||
+        oauth_tokens!(impl)
+
+    access_token
+  end
+
+  def refresh_access_token!(impl) do
+    refresh_token =
+      get_from_cache(fn cache -> cache[impl] end) || impl.get_refresh_token_from_env()
+
+    refresh_access_token!(impl, refresh_token)
+  end
+
+  defp refresh_access_token!(_impl, nil), do: nil
+
+  defp refresh_access_token!(impl, %{refresh_token: refresh_token}),
+    do: impl.refresh_access_token!(refresh_token)
+
+  defp refresh_access_token!(impl, refresh_token) when is_binary(refresh_token),
+    do: impl.refresh_access_token!(refresh_token)
+
+  def oauth_tokens!(impl) do
     uri = impl.uri()
     code = get_code!(uri)
-    impl.exchange_code!(code)
+    %{access_token: access_token, refresh_token: refresh_token} = impl.exchange_code!(code)
+
+    tokens = %{access_token: access_token, refresh_token: refresh_token}
+
+    store_in_cache(fn cache -> Map.put(cache, impl, tokens) end)
+
+    tokens
   end
 
   defp get_code!(uri) do
@@ -22,6 +59,22 @@ defmodule OAuth do
     receive do
       {:callback, code} -> code
       _ -> receive_code()
+    end
+  end
+
+  defp get_from_cache(get_fn) do
+    Agent.get(agent(), get_fn)
+  end
+
+  defp store_in_cache(update_fn) do
+    Agent.update(agent(), update_fn)
+  end
+
+  # Use a local agent as a cache system
+  defp agent() do
+    case Agent.start_link(fn -> %{} end, name: __MODULE__) do
+      {:ok, pid} -> pid
+      {:error, {:already_started, pid}} -> pid
     end
   end
 end
--- lib/youtube/oauth.ex
+++ lib/youtube/oauth.ex
@@ -6,35 +6,34 @@ defmodule Youtube.OAuth do
   @client_id Application.compile_env!(:roule_avec_spot, [Youtube, :client_id])
   @client_secret Application.compile_env!(:roule_avec_spot, [Youtube, :client_secret])
   @redirect_uri Application.compile_env!(:roule_avec_spot, [Youtube, :redirect_uri])
+  @refresh_token Application.compile_env(:roule_avec_spot, [Youtube, :refresh_token])
 
-  def get_token() do
-    token = read_token()
-
-    cond do
-      token ->
-        token
-
-      true ->
-        token = OAuth.create_access_token!(Youtube.OAuth)
-        Application.put_env(:roule_avec_spot, :google_token, token)
-        token
-    end
+  def access_token!() do
+    OAuth.access_token!(Youtube.OAuth)
   end
 
-  def read_token() do
-    Application.get_env(:roule_avec_spot, :google_token)
+  @impl OAuth
+  def get_refresh_token_from_env() do
+    @refresh_token
+  end
+
+  @impl OAuth
+  def refresh_access_token!(refresh_token) do
+    %{"access_token" => access_token} =
+      make_oauth_request(%{
+        "client_id" => @client_id,
+        "client_secret" => @client_secret,
+        "refresh_token" => refresh_token,
+        "grant_type" => "refresh_token"
+      })
+
+    %{access_token: access_token}
   end
 
   @impl OAuth
   def exchange_code!({:youtube, code}) do
-    client =
-      Tesla.client([
-        Tesla.Middleware.FormUrlencoded
-        # Tesla.Middleware.Logger
-      ])
-
-    %{status: 200, body: body} =
-      post!(client, "https://oauth2.googleapis.com/token", %{
+    %{"access_token" => access_token, "refresh_token" => refresh_token} =
+      make_oauth_request(%{
         "client_id" => @client_id,
         "client_secret" => @client_secret,
         "redirect_uri" => @redirect_uri,
@@ -42,8 +41,19 @@ defmodule Youtube.OAuth do
         "grant_type" => "authorization_code"
       })
 
-    %{"access_token" => token} = Jason.decode!(body)
-    token
+    %{access_token: access_token, refresh_token: refresh_token}
+  end
+
+  defp make_oauth_request(params) do
+    client =
+      Tesla.client([
+        Tesla.Middleware.FormUrlencoded
+        # Tesla.Middleware.Logger
+      ])
+
+    %{status: 200, body: body} = post!(client, "https://oauth2.googleapis.com/token", params)
+
+    Jason.decode!(body)
   end
 
   @impl OAuth
@@ -53,6 +63,8 @@ defmodule Youtube.OAuth do
         "client_id" => @client_id,
         "redirect_uri" => @redirect_uri,
         "response_type" => "code",
+        "access_type" => "offline",
+        "prompt" => "consent",
         "scope" => Enum.join(["https://www.googleapis.com/auth/youtube.readonly"], " ")
       })
 
@@ -64,7 +76,10 @@ defmodule Youtube.OAuth do
 
     def call(env, next, _) do
       env
-      |> Map.update!(:headers, &[{"Authorization", "Bearer #{Youtube.OAuth.get_token()}"} | &1])
+      |> Map.update!(
+        :headers,
+        &[{"Authorization", "Bearer #{Youtube.OAuth.access_token!()}"} | &1]
+      )
       |> Tesla.run(next)
     end
   end
