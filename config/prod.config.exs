99bcc874763c8b1ff3028909d170c5201e1dba6b
daniel <dans44684@hotmail.com>
2023-01-02 14:52:47 +0900

    Add prod config

---
+++ config/prod.config.exs
@@ -0,0 +1,13 @@
+import Config
+
+config :roule_avec_spot, Youtube,
+  client_secret: System.get_env("YOUTUBE_OAUTH_CLIENT_SECRET"),
+  client_id: System.get_env("YOUTUBE_OAUTH_CLIENT_ID"),
+  redirect_uri: System.get_env("YOUTUBE_OAUTH_CLIENT_REDIRECT_URI"),
+  channel_id: System.get_env("YOUTUBE_CHANNEL_ID")
+
+config :roule_avec_spot, Spotify,
+  client_secret: System.get_env("SPOTIFY_OAUTH_CLIENT_SECRET"),
+  client_id: System.get_env("SPOTIFY_OAUTH_CLIENT_ID"),
+  redirect_uri: System.get_env("SPOTIFY_OAUTH_CLIENT_REDIRECT_URI"),
+  redirect_uri: System.get_env("SPOTIFY_USER_ID")
