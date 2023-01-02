d3c99df1f16c84e1c2fa3cffa35c508be85c2c91
daniel <dans44684@hotmail.com>
2023-01-02 18:45:16 +0900

    Use fetch_env to fail fast

--- config/prod.config.exs
+++ config/prod.config.exs
@@ -1,13 +1,13 @@
 import Config
 
 config :roule_avec_spot, Youtube,
-  client_secret: System.get_env("YOUTUBE_OAUTH_CLIENT_SECRET"),
-  client_id: System.get_env("YOUTUBE_OAUTH_CLIENT_ID"),
+  client_secret: System.fetch_env!("YOUTUBE_OAUTH_CLIENT_SECRET"),
+  client_id: System.fetch_env!("YOUTUBE_OAUTH_CLIENT_ID"),
   redirect_uri: System.get_env("YOUTUBE_OAUTH_CLIENT_REDIRECT_URI"),
   channel_id: System.get_env("YOUTUBE_CHANNEL_ID")
 
 config :roule_avec_spot, Spotify,
-  client_secret: System.get_env("SPOTIFY_OAUTH_CLIENT_SECRET"),
-  client_id: System.get_env("SPOTIFY_OAUTH_CLIENT_ID"),
+  client_secret: System.fetch_env!("SPOTIFY_OAUTH_CLIENT_SECRET"),
+  client_id: System.fetch_env!("SPOTIFY_OAUTH_CLIENT_ID"),
   redirect_uri: System.get_env("SPOTIFY_OAUTH_CLIENT_REDIRECT_URI"),
-  user_id: System.get_env("SPOTIFY_USER_ID")
+  user_id: System.fetch_env!("SPOTIFY_USER_ID")
