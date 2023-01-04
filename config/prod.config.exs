8c0982c37f74affa2933ad4d58d4ec0eaccbe4b8
daniel <dans44684@hotmail.com>
2023-01-05 07:55:23 +0900

    Add refresh token to CI env

--- config/prod.config.exs
+++ config/prod.config.exs
@@ -3,13 +3,13 @@ import Config
 config :roule_avec_spot, Youtube,
   client_secret: System.fetch_env!("YOUTUBE_OAUTH_CLIENT_SECRET"),
   client_id: System.fetch_env!("YOUTUBE_OAUTH_CLIENT_ID"),
-  redirect_uri: System.get_env("YOUTUBE_OAUTH_CLIENT_REDIRECT_URI"),
+  refresh_token: System.get_env("YOUTUBE_OAUTH_REFRESH_TOKEN"),
   channel_id: System.get_env("YOUTUBE_CHANNEL_ID")
 
 config :roule_avec_spot, Spotify,
   client_secret: System.fetch_env!("SPOTIFY_OAUTH_CLIENT_SECRET"),
   client_id: System.fetch_env!("SPOTIFY_OAUTH_CLIENT_ID"),
-  redirect_uri: System.get_env("SPOTIFY_OAUTH_CLIENT_REDIRECT_URI"),
+  refresh_token: System.get_env("SPOTIFY_OAUTH_REFRESH_TOKEN"),
   user_id: System.fetch_env!("SPOTIFY_USER_ID")
 
 config :roule_avec_spot, Line, channel_token: System.fetch_env!("LINE_CHANNEL_TOKEN")
